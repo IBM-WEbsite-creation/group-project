b02a0a1aada64539ebe857f41f4a25ba35327f0c
Lakshmi <lakshmisowparnikam@gmail.com>
2025-07-29 20:09:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("donationForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const item = document.getElementById("item").value;
+  const location = document.getElementById("location").value;
+
+  if (name && item && location) {
+    document.getElementById("donationForm").reset();
+    document.getElementById("thankYouMsg").style.display = "block";
+  }
+});
